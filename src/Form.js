fa1bb9afbde081345f5024c5a52d4e7f9c8284d7
Mikhail <ryabushkin.mikhail@gmail.com>
2018-12-23 21:12:17 +0300

    create validation

---
+++ src/Form.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import FormErrors from './FormErrors';
+import styled from 'styled-components';
+
+const FormErrorsWrapper = styled.div``;
+
+export default class Form extends Component {
+  state = {
+    email: '',
+    password: '',
+    formErrors: {
+      email: '',
+      password: '',
+    },
+    emailValid: false,
+    passwordValid: false,
+    formValid: false,
+  };
+
+  validateForm = () => {
+    this.setState({
+      formValid: this.state.emailValid && this.state.passwordValid,
+    });
+  };
+
+  validateField = (fieldName, value) => {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch (fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid
+          ? ''
+          : ' is invalid, please enter email like username@hostname';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '' : ' is too short';
+        break;
+      default:
+        break;
+    }
+
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        emailValid,
+        passwordValid,
+      },
+      this.validateForm,
+    );
+  };
+
+  handleUserInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.validateField(name, value),
+    );
+  };
+
+  errorClass = error => {
+    return error.length === 0 ? '' : 'has-error';
+  };
+
+  render() {
+    const { email, password, formErrors, formValid } = this.state;
+    return (
+      <form className="container">
+        <h2>Sign up</h2>
+        <FormErrorsWrapper>
+          <FormErrors formErrors={formErrors} />
+        </FormErrorsWrapper>
+        <div className={`form-group ${this.errorClass(formErrors.email)}`}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={this.handleUserInput}
+          />
+        </div>
+        <div className={`form-group ${this.errorClass(formErrors.password)}`}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            value={password}
+            onChange={this.handleUserInput}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" disabled={!formValid}>
+          Sign up
+        </button>
+      </form>
+    );
+  }
+}
---
+++ src/FormErrors.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Error = styled.p`
+  padding: 1rem;
+  margin: 0 0 1rem 0;
+  color: white;
+  background-color: tomato;
+`;
+
+export default ({ formErrors }) => (
+  <React.Fragment>
+    {Object.keys(formErrors).map(fieldName => {
+      if (formErrors[fieldName].length > 0) {
+        return (
+          <Error key={fieldName}>
+            {fieldName} {formErrors[fieldName]}
+          </Error>
+        );
+      } else {
+        return '';
+      }
+    })}
+  </React.Fragment>
+);
